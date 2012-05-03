dd1f9b10ab80aeed046af5148d55f3a6104da982
quickstart <quickstart@quickstart.(none)>
2012-05-02 17:19:22 -0700

    shorter sleep.  change log to logs folder

--- install-quickstart.sh
+++ install-quickstart.sh
@@ -15,7 +15,7 @@ echo "qs1204" | sudo tee /etc/hostname
 function reboot {
   # update .profile file to continue the next step of the script.
   echo "gnome-terminal -x bash -c \"~/quickstart/install-quickstart.sh $1\" &" >> ~/.profile
-  echo "*** REBOOTING ***" | tee -a ~/quickstart/quickstart-install.log
+  echo "*** REBOOTING ***" | tee -a ~/quickstart/logs/quickstart-install.log
   sleep 2
   sudo reboot now
   exit
@@ -27,34 +27,34 @@ sed -i 's/gnome-terminal -x bash -c/# deleteme /g' ~/.profile
 # ################################################################################ Install it!
 # this switch statement handles reboots.
 cd ~
-  # @FIXME: change 'sleep 30' to test for active network connection before coninuing
-  echo "sleep 30"
+  # @FIXME: change 'sleep 15' to actually test for active network connection before continuing
+  echo "sleep 15"
   sleep 30
 
 case "$1" in
 "")
-  bash -x ~/quickstart/quickstart-1-prep.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
-  bash -x ~/quickstart/quickstart-1a-guest.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
+  bash -x ~/quickstart/quickstart-1-prep.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
+  bash -x ~/quickstart/quickstart-1a-guest.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
   reboot 2
   ;;
 "2")
-  bash -x ~/quickstart/quickstart-2-slim.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
-  bash -x ~/quickstart/quickstart-2a-update.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
+  bash -x ~/quickstart/quickstart-2-slim.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
+  bash -x ~/quickstart/quickstart-2a-update.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
   reboot 3
   ;;
 "3")
-  bash -x ~/quickstart/quickstart-3-lamp.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
-  bash -x ~/quickstart/quickstart-4-ides.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
+  bash -x ~/quickstart/quickstart-3-lamp.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
+  bash -x ~/quickstart/quickstart-4-ides.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
   reboot 5
   ;;
 "5")
-  bash -x ~/quickstart/quickstart-5-browsers.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
-  bash -x ~/quickstart/quickstart-6-devenv.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
-  bash -x ~/quickstart/quickstart-7-config.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
+  bash -x ~/quickstart/quickstart-5-browsers.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
+  bash -x ~/quickstart/quickstart-6-devenv.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
+  bash -x ~/quickstart/quickstart-7-config.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
   reboot 8
   ;;
 "8")
-  bash -x ~/quickstart/quickstart-8-manualconfig.sh  2>&1 | tee -a ~/quickstart/quickstart-install.log
+  bash -x ~/quickstart/quickstart-8-manualconfig.sh  2>&1 | tee -a ~/quickstart/logs/quickstart-install.log
   ;;
 *)
   echo " *** BAD BAD BAD SOMETHING WENT WRONG!  CALL A DOCTOR! *** "
