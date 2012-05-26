c241f7c7cb717570700d116f0ff3c39ee927c7e5
Mike Stewart <mdrmike@30158.no-reply.drupal.org>
2012-05-26 11:12:37 -0700

    ADD configurations for additional cleanup

--- setup_scripts/export-prep.sh
+++ setup_scripts/export-prep.sh
@@ -16,7 +16,18 @@ sudo rm /var/lib/apt/lists/partial/*
 sudo rm -rf ~/.local/share/Trash/files/*
 sudo rm -rf ~/.local/share/Trash/info/*
 
+#clear bash history
+cat /dev/null > ~/.bash_history
+cat /dev/null > ~/.bash_eternal_history
+
+#clear gnome history
+cat /dev/null > ~/.local/share/recently-used.xbel
+
+#clear logs
+sudo find /var/log/ -name '*.gz' -type f -print0 -exec rm '{}' \;
+sudo logrotate -f -s /home/quickstart/quickstart/setup_scripts/logs/logrotate-status.log /home/quickstart/config/clear-all-logs.conf
+
 # Zero-fill unused sectors on vm disk
-# Zero-filled sectors compress very nice :-)  
+# Zero-filled sectors compress very nice :-)
 # No need to export sectors for files that could be "undeleted"
 sudo dd if=/dev/zero of=/zerofile; sudo rm /zerofile
