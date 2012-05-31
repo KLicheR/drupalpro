af962905ed0e39e679629c4a36760e55043a450c
Mike Stewart <mdrmike@30158.no-reply.drupal.org>
2012-05-31 06:57:10 -0700

    CHANGE firefox instructions and order of zenity

--- setup_scripts/8-manualconfig.sh
+++ setup_scripts/8-manualconfig.sh
@@ -5,13 +5,19 @@ cd ~/websites
 drush qc --domain=example7.dev
 drush qc --domain=example6.dev --makefile=d6.make
 
-#zenity --info --text="Firefox will start (creating default profile).\n\nPlease CLOSE FIREFOX. " &
-firefox
-firefox -CreateProfile temp
+
+firefox &
+firefox -CreateProfile temp &
+zenity --info --text="Firefox will start to create profiles.\n\nPlease CLOSE FIREFOX. "
 #zenity --info --text="Firefox will start.\n\n1) Please install the FEBE backup extension.\n2) Tools -> FEBE -> Restore profile.\n3) Click 'default' in list (create if it's not there).\n4) Select button at bottom 'Restore local backup'.\n5) Choose file: ~/profileFx4{default}.fbu.\n6) Start profile restore.\n7) Ok, ok, then CLOSE FIREFOX" &
 #firefox -P temp https://addons.mozilla.org/en-US/firefox/downloads/latest/2109/addon-2109-latest.xpi?src=addondetail
 firefox -P temp http://softwarebychuck.com/xpis/FEBE7.0.3.3.xpi &
-zenity --info --text="Firefox is starting in order to create default and temp profiles.  Then Firefox will attempt to install the FEBE extention. Once FEBE is installed, choose the option to RESTART Firefox.  Next, choose Tools > FEBE > Restore Profile to restore the default profile using the file: ~/profileFx4{default}.fbu.\n  Finally, close Firefox "
+zenity --info --text="Firefox is starting in order to install FEBE.
+
+1) Install the FEBE extention.
+2) Once FEBE is installed, choose the option to RESTART Firefox.
+3) Next, choose Tools > FEBE > Restore Profile to restore the default profile using the file: ~/profileFx4{default}.fbu.\n
+4) Finally, close Firefox "
 
 firefox -ProfileManager &
 zenity --info --text="Firefox profile manager will start.\n1) Delete temp profile.\n2) THEN CLOSE MANAGER."
