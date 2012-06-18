74150577ae18e001dd15f1dffda4f13bf9300fd8
Mike Stewart <mdrmike@30158.no-reply.drupal.org>
2012-06-18 02:26:36 -0700

    CLEANUP firefox install

--- setup_scripts/8-manualconfig.sh
+++ setup_scripts/8-manualconfig.sh
@@ -12,12 +12,13 @@ drush qc --domain=example7.dev
 drush qc --domain=example6.dev --makefile=d6.make
 
 
-firefox &
 firefox -CreateProfile temp &
-zenity --info --text="Firefox will start to create profiles.\n\nPlease CLOSE FIREFOX. "
-#zenity --info --text="Firefox will start.\n\n1) Please install the FEBE backup extension.\n2) Tools -> FEBE -> Restore profile.\n3) Click 'default' in list (create if it's not there).\n4) Select button at bottom 'Restore local backup'.\n5) Choose file: ~/profileFx4{default}.fbu.\n6) Start profile restore.\n7) Ok, ok, then CLOSE FIREFOX" &
-#firefox -P temp https://addons.mozilla.org/en-US/firefox/downloads/latest/2109/addon-2109-latest.xpi?src=addondetail
+sleep 10
+firefox -CreateProfile Default &
+sleep 5
+zenity --info --text="Firefox started in order to create profiles.\n\nPlease CLOSE FIREFOX. "
 firefox -P temp http://softwarebychuck.com/xpis/FEBE7.0.3.3.xpi &
+sleep 5
 zenity --info --text="Firefox is starting in order to install FEBE.
 
 1) Install the FEBE extention.
@@ -26,8 +27,9 @@ zenity --info --text="Firefox is starting in order to install FEBE.
 4) Finally, close Firefox "
 
 firefox -ProfileManager &
+sleep 5
 zenity --info --text="Firefox profile manager will start.\n1) Delete temp profile.\n2) THEN CLOSE MANAGER."
-rm profileFx4{default}.fbu
+#rm profileFx4{ddd}.fbu
 
 # final size
 df -h -T > ~/$DDD/setup_scripts/logs/size-end.log
