319a8ff6f61509392e60c2fa8048b9c963d4e3ea
Mike Stewart <mdrmike@30158.no-reply.drupal.org>
2012-06-28 20:27:36 -0700

    ADD some useful GIT configurations.  Thanks for suggesting in IRC TravisCarden!

--- setup_scripts/extras_misc.sh
+++ setup_scripts/extras_misc.sh
@@ -8,9 +8,40 @@ if [[ ${DEBUG} == true ]]; then set -x -v; fi
 
 # add some useful git tools
 sudo apt-get ${APTGET_VERBOSE} install gitg meld git-gui gitk
-# configure some nice git settings
-git config --global color.ui true
-git config --global core.whitespace trailing-space,tab-in-indent
+# mostly based off http://cheat.errtheblog.com/s/git
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.df diff
+git config --global alias.lg "log --graph --pretty=format:'%C(blue)%h %Creset%C(reverse bold blue)%d%Creset %C(white)%s %C(green bold)%cr%Creset %C(green)%aN' --abbrev-commit --decorate"
+# can't seem to isolate ! in command > git config --global alias.clear "!git add -A && git reset --hard"
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.ign "ls-files -o -i --exclude-standard"
+# can't seem to isolate ! in command > git config --global alias.gitkconflict "!gitk --left-right HEAD...MERGE_HEAD"
+git config --global alias.alias "config --get-regexp alias"
+git config --global apply.whitespace error-all
+git config --global color.ui auto
+git config --global color.diff.whitespace "red reverse"
+git config --global color.diff.meta "magenta"
+git config --global color.diff.frag "yellow"
+git config --global color.diff.old "red"
+git config --global color.diff.new "green bold"
+git config --global color.grep.context yellow
+git config --global color.grep.filename blue
+git config --global color.grep.function "yellow bold"
+git config --global color.grep.linenumber "cyan bold"
+git config --global color.grep.match red bold
+git config --global color.grep.selected white
+git config --global color.grep.separator blue
+git config --global color.status.added yellow
+git config --global color.status.changed red
+git config --global color.status.untracked "cyan bold"
+git config --global diff.tool meld
+git config --global gui.editor geany
+git config --global merge.summary true
+git config --global merge.tool meld
+
 
 # Install some useful utilities for developing & theming in Ubuntu
 # Synaptic Xchat gnote compass guake (instant shell) gufw (GUI for firewall)
