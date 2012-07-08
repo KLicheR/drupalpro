003738b03af24f2a70a37fa66dcd3a6fd5269e14
Mike Stewart <mdrmike@30158.no-reply.drupal.org>
2012-07-08 15:42:04 -0700

    IMPLEMENT installation variables

--- setup_scripts/0-slim.sh
+++ setup_scripts/0-slim.sh
@@ -11,38 +11,57 @@ if [[ ${DEBUG} == true ]]; then set -x -v; fi
 # 0mb
 sudo apt-get ${APTGET_VERBOSE} autoremove
 
-# 305mb - Libre Office
-sudo apt-get ${APTGET_VERBOSE} purge libreoffice-gnome  libreoffice-draw  libreoffice-help-en-us  libreoffice-style-tango  libreoffice-impress  libreoffice-math  libreoffice-gtk  libreoffice-common  uno-libs3  python-uno  libreoffice-style-human  ure  libreoffice-base-core  libreoffice-calc  libreoffice-emailmerge  libreoffice-core  libreoffice-writer
-#  18mb - Games
-sudo apt-get ${APTGET_VERBOSE} purge aisleriot gnome-sudoku mahjongg gnomine
-#   422k - Screen savers
-sudo apt-get ${APTGET_VERBOSE} purge gnome-screensaver
-# 61mb - User guide
-#mdrmike @FIXME:(purge) |keep help sudo apt-get ${APTGET_VERBOSE} purge gnome-user-guide ubuntu-docs
-#   6mb - Bluetooth
-sudo apt-get ${APTGET_VERBOSE} purge bluez-alsa gir1.2-gnomebluetooth-1.0 bluez-gstreamer pulseaudio-module-bluetooth bluez-cups bluez gnome-bluetooth gnome-user-share
-#  3.2mb - speech synthesis
-sudo apt-get ${APTGET_VERBOSE} purge espeak speech-dispatcher espeak-data libespeak1
-#  67.5mb - social, IM, mail
-sudo apt-get ${APTGET_VERBOSE} purge telepathy-indicator folks-common telepathy-logger telepathy-gabble gwibber-service-identica thunderbird telepathy-haze empathy-common gwibber-service-twitter gwibber-service-facebook telepathy-idle empathy telepathy-salut libgwibber2 gwibber-service libfolks-eds25 thunderbird-globalmenu thunderbird-gnome-support gwibber libgwibber-gtk2 nautilus-sendto-empathy telepathy-mission-control-5
-#  63mb - Printing
-#mdrmike @FIXME:(purge) |too useful sudo apt-get ${APTGET_VERBOSE} purge cups cups-bsd cups-client cups-common ghostscript ghostscript-x ghostscript-cups cups-driver-gutenprint python-cups system-config-printer-common system-config-printer-gnome system-config-printer-udev foo2zjs foomatic-db-engine foomatic-filters min12xxw openprinting-ppds pnm2ppa pxljr splix hplip-data hplip hpijs libcupsmime1 libcupsdriver1 libgutenprint2 libcupsppdc1
-# 15mb - Scanner drivers
-#mdrmike @FIXME:(purge) |too integrated with printing sudo apt-get ${APTGET_VERBOSE} purge sane-utils simple-scan libsane libsane-hpaio
-#  6.9mb - Shotwell photo manager
-sudo apt-get ${APTGET_VERBOSE} purge shotwell
-#   8mb - Fancy GUI Compiz
-#mdrmike @FIXME:(purge) |too entwined with unity sudo apt-get ${APTGET_VERBOSE} purge compiz compiz-core compiz-gnome compiz-plugins compizconfig-backend-gconf libcompizconfig0 libdecoration0
-#   6mb - Example videos and stuff
-sudo apt-get ${APTGET_VERBOSE} purge example-content
-#   1.7mb - Bittorrent client
-sudo apt-get ${APTGET_VERBOSE} purge transmission-common transmission-gtk
-#   11mb - ubuntuone, rhythymbox, remotedesktop
-sudo apt-get ${APTGET_VERBOSE} purge gir1.2-rb-3.0 gir1.2-ubuntuoneui-3.0 rhythmbox-data ubuntuone-client-gnome ubuntuone-couch ubuntuone-control-panel ubuntuone-installer rhythmbox-ubuntuone libubuntuoneui-3.0-1 rhythmbox librhythmbox-core5 rhythmbox-plugin-magnatune unity-lens-video unity-scope-video-remote unity-scope-musicstores unity-lens-music libsyncdaemon-1.0-1 vino remmina-common rhythmbox-mozilla remmina-plugin-rdp rhythmbox-plugin-zeitgeist remmina-plugin-vnc ubuntuone-client-gnome rhythmbox-plugin-cdrecorder rhythmbox-plugins remmina
-#   5mb - Etc
-sudo apt-get ${APTGET_VERBOSE} purge usb-creator-gtk checkbox-gtk jockey-gtk #computer-janitor-gtk
+if [ "${PURGE_OFFICE}" = true ]; then
+  # 305mb - Libre Office
+  sudo apt-get ${APTGET_VERBOSE} purge libreoffice-gnome  libreoffice-draw  libreoffice-help-en-us  libreoffice-style-tango  libreoffice-impress  libreoffice-math  libreoffice-gtk  libreoffice-common  uno-libs3  python-uno  libreoffice-style-human  ure  libreoffice-base-core  libreoffice-calc  libreoffice-emailmerge  libreoffice-core  libreoffice-writer
+fi
+if [ "${PURGE_GAMES}" = true ]; then
+  #  18mb - Games
+  sudo apt-get ${APTGET_VERBOSE} purge aisleriot gnome-sudoku mahjongg gnomine
+fi
+if [ "${PURGE_BLUETOOTH}" = true ]; then
+  #   6mb - Bluetooth
+  sudo apt-get ${APTGET_VERBOSE} purge bluez-alsa gir1.2-gnomebluetooth-1.0 bluez-gstreamer pulseaudio-module-bluetooth bluez-cups bluez gnome-bluetooth gnome-user-share
+fi
+if [ "${PURGE_MEDIA}" = true ]; then
+  #  6.9mb - Shotwell photo manager
+  sudo apt-get ${APTGET_VERBOSE} purge shotwell
+  #   6mb - Example videos and stuff
+  sudo apt-get ${APTGET_VERBOSE} purge example-content
+  #   rhythymbox
+  sudo apt-get ${APTGET_VERBOSE} purge rhythmbox rhythmbox-data rhythmbox-ubuntuone librhythmbox-core5 rhythmbox-plugin-magnatune gir1.2-rb-3.0 rhythmbox-mozilla rhythmbox-plugin-zeitgeist rhythmbox-plugin-cdrecorder rhythmbox-plugins
+  #   Unity media lens
+  sudo apt-get ${APTGET_VERBOSE} purge unity-lens-video unity-scope-video-remote unity-scope-musicstores unity-lens-music
+fi
+if [ "${PURGE_MISC}" = true ]; then
+  #   422k - Screen savers
+  sudo apt-get ${APTGET_VERBOSE} purge gnome-screensaver
+  #  3.2mb - speech synthesis
+  sudo apt-get ${APTGET_VERBOSE} purge espeak speech-dispatcher espeak-data libespeak1
+  #   1.7mb - Bittorrent client
+  sudo apt-get ${APTGET_VERBOSE} purge transmission-common transmission-gtk
+  #   ubuntuone
+  sudo apt-get ${APTGET_VERBOSE} purge ubuntuone-client-gnome ubuntuone-client-gnome gir1.2-ubuntuoneui-3.0 libsyncdaemon-1.0-1 ubuntuone-couch ubuntuone-control-panel libubuntuoneui-3.0-1
+  #   remotedesktop
+  sudo apt-get ${APTGET_VERBOSE} purge vino remmina-common remmina-plugin-rdp remmina-plugin-vnc remmina
+  #   Etc
+  sudo apt-get ${APTGET_VERBOSE} purge usb-creator-gtk checkbox-gtk jockey-common jockey-gtk #computer-janitor-gtk
+fi
+if [ "${PURGE_SOCIAL}" = true ]; then
+  #  67.5mb - social, IM, mail
+  sudo apt-get ${APTGET_VERBOSE} purge telepathy-indicator folks-common telepathy-logger telepathy-gabble gwibber-service-identica thunderbird telepathy-haze empathy-common gwibber-service-twitter gwibber-service-facebook telepathy-idle empathy telepathy-salut libgwibber2 gwibber-service libfolks-eds25 thunderbird-globalmenu thunderbird-gnome-support gwibber libgwibber-gtk2 nautilus-sendto-empathy telepathy-mission-control-5
+fi
+if [ "${PURGE_HELP}" = true ]; then
+  # User guide
+  sudo apt-get ${APTGET_VERBOSE} purge gnome-user-guide ubuntu-docs
+fi
+if [ "${PURGE_PRINT}" = true ]; then
+  #  63mb - Printing
+  sudo apt-get ${APTGET_VERBOSE} purge cups cups-bsd cups-client cups-common ghostscript ghostscript-x ghostscript-cups cups-driver-gutenprint python-cups system-config-printer-common system-config-printer-gnome system-config-printer-udev foo2zjs foomatic-db-engine foomatic-filters min12xxw openprinting-ppds pnm2ppa pxljr splix hplip-data hplip hpijs libcupsmime1 libcupsdriver1 libgutenprint2 libcupsppdc1
+  # 15mb - Scanner drivers
+  sudo apt-get ${APTGET_VERBOSE} purge sane-utils simple-scan libsane libsane-hpaio
+fi
 
-# 0mb
 sudo apt-get ${APTGET_VERBOSE} autoremove # autoremove is used to remove packages that were automatically installed to satisfy dependencies for other packages and are now no longer needed.
 sudo apt-get ${APTGET_VERBOSE} clean # clean clears out the local repository of retrieved package files. It removes everything but the lock file from /var/cache/apt/archives/ and /var/cache/apt/archives/partial/.
 
