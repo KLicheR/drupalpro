3922d24e310cbdf84012086386dae2ec67bdd760
Kristoffer LR <kristoffer.laurin-racicot@ville.montreal.qc.ca>
2012-11-09 13:48:34 -0500

    Add a "mkdir" for the app folder before the Drush installation.

--- setup_scripts/6-extras_development.sh
+++ setup_scripts/6-extras_development.sh
@@ -57,8 +57,7 @@ For more information:
 
 #======================================| Drush
 if [[ ${INSTALL_DRUSH} == true ]]; then
-  # Install drush
-
+  mkdir -p "${APP_FOLDER}"
   cd ${APP_FOLDER}
   git clone http://git.drupal.org/project/drush.git
   cd ${APP_FOLDER}/drush
